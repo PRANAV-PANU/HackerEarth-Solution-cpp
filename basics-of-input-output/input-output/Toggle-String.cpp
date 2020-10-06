3cc6de1bd8fa6739dc571ee2d66636f084f3cab7
Pranav Agarwal <pranav.juit@gmail.com>
2020-10-06 12:54:04 +0530

    cpp code

---
+++ basics-of-input-output/input-output/Toggle-String.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    int n =s.length();
+    for(int i=0;i<n;i++){
+        if(isupper(s[i]))
+            s[i]=tolower(s[i]);
+        else
+            s[i]=toupper(s[i]);    
+    }
+    cout<<s;
+    return 0;
+}
